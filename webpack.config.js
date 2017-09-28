d93b8afceaf511b660233c8e0d033344a6993901
Adel <shadel.shsh@gmail.com>
2017-09-28 04:34:39 +0000

    init

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry : './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'index_bundle.js'
+	},
+ 	devtool: 'inline-source-map',
+	module: {
+		rules: [
+// 			{ test: /\.(js)$/, use: 'babel-loader'},
+			{ test: /\.css$/, use: [ 'style-loader', 'css-loader']},
+			{test: /\.js$/,include: /linebreak/,loader: "transform-loader?brfs"}
+		]
+	},
+	plugins: [new HtmlWebpackPlugin({
+		template: 'src/template/index.html'
+	})],
+	
+	devServer: {
+
+    compress: true,
+
+    disableHostCheck: true,   // That solved it
+
+     }      
+
+}
